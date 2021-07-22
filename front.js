8bf39e4193bd679b4df8c13a7a7ffc59dc5bd13c
Prakash-Parmar <80259553+Prakash-Parmar@users.noreply.github.com>
2021-07-22 21:21:01 +0530

    Add files via upload

---
+++ front.js
@@ -0,0 +1,17 @@
+// alert("Welcome to simple portfolio site");
+
+var links = document.getElementsByClassName("nav-link");
+var news_link = links[1];
+news_link.onclick = function(){
+    alert("News link is not working");
+}
+
+var signup_link = links[4];
+signup_link.onclick = function(){
+    alert("Signup link is not working");
+}
+
+var login_link = links[5];
+login_link.onclick = function(){
+    alert("Login link is not working");
+}
